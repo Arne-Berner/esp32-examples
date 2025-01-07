92271211f3d9781e131a032f00d50a045adef51d
Arne Berner <info@arneberner.de>
2025-01-07 14:36:05 +0100

    feat: changed the led lib to work with more cases

--- examples/led_lib.rs
+++ examples/led_lib.rs
@@ -1,24 +1,24 @@
 use esp_idf_svc::hal::gpio::{Output, OutputPin, Pin, PinDriver};
 
 pub struct LED<'a, S: Pin> {
-    led: PinDriver<'a, S, Output>,
+    led_driver: PinDriver<'a, S, Output>,
 }
 
 impl<S> LED<'_, S>
 where
     S: OutputPin,
 {
-    pub fn new(led_gpio: S) -> anyhow::Result<Self, anyhow::Error> {
-        let led = PinDriver::output(led_gpio)?;
-        Ok(Self { led })
+    pub fn new(led: S) -> anyhow::Result<Self, anyhow::Error> {
+        let led_driver = PinDriver::output(led)?;
+        Ok(Self { led_driver })
     }
 
     // Get this out of here
     pub fn set_led(self: &mut Self, high: bool) -> anyhow::Result<(), anyhow::Error> {
         if high {
-            self.led.set_high()?;
+            self.led_driver.set_high()?;
         } else {
-            self.led.set_low()?;
+            self.led_driver.set_low()?;
         }
         Ok(())
     }
